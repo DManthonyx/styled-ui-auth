497fb57caecb227da7ab5c916f82d7e5811da8c5
Dmanthonyx <dmanthonycoding@gmail.com>
2022-11-19 08:54:50 -0800

    added typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
+import typescript from "@rollup/plugin-typescript";
 import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
 import postcss from 'rollup-plugin-postcss';
 
 export default [
     {
-        input: './src/index.js',
+        input: './src/index.ts',
         output: [
             {
                 file: 'dist/index.js',
@@ -28,9 +30,11 @@ export default [
                 exclude: 'node_modules/**',
                 presets: ['@babel/preset-react']
             }),
+            typescript({ tsconfig: "./tsconfig.json" }),
             external(),
             resolve(),
             terser(),
+            commonjs(),
         ]
     }
-]
+];
